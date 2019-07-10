7a42d2f267c1dee11b2385c998f705021f1a5c2a
back1992 <back1992@gmail.com>
2019-07-10 09:06:20 +0800

    .gitignore fix

--- stop_in_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# sudo cp ./model/client/CTP/linux/x64/lib* /usr/local/lib
-# sudo cp ./model/client/Sgit/linux/x64/lib* /usr/local/lib
-# sudo npm install
-# sudo npm install forever -g
-fuser -k -n tcp 3000
-/usr/local/bin/forever stop 0
