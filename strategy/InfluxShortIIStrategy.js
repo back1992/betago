b49b91234176f060e7a8c2c04675b045b0b33e34
back1992 <linmukong@iCloud.com>
2019-08-01 10:35:58 +0800

    gold keep use closedbar as flag avoid too many query account

--- strategy/InfluxShortIIStrategy.js
+++ strategy/InfluxShortIIStrategy.js
@@ -10,53 +10,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 var BaseStrategy = require("./baseStrategy");
-//
-// function _get_signal(mfi, cci, cmo, aroonosc, adx, rsi) {
-//     let score = 0;
-//     if (cci > 100) {
-//         score -= 1
-//     } else if (cci < -100) {
-//         score += 1
-//     }
-//     if (mfi > 80) {
-//         score -= 1
-//     } else if (mfi < 20) {
-//         score += 1
-//     }
-//     if (cmo > 50) {
-//         score -= 1
-//     } else if (cmo < -50) {
-//         score += 1
-//     }
-//     if (aroonosc < -50) {
-//         score -= 1
-//     } else if (aroonosc > 50) {
-//         score += 1
-//     }
-//     if (rsi > 70) {
-//         score -= 1
-//     } else if (rsi < 30) {
-//         score += 1
-//     }
-//     return score
-// }
-//
-// function _get_talib_indicator(strategy, highPrice, lowPrice, closePrice, volume) {
-//     let retMFI = talib.MFI(highPrice, lowPrice, closePrice, volume, 14);
-//     let retCCI = talib.CCI(highPrice, lowPrice, closePrice, 14);
-//     let retCMO = talib.CMO(closePrice, 14);
-//     let retAROONOSC = talib.AROONOSC(highPrice, lowPrice, 14);
-//     let retADX = talib.ADX(highPrice, lowPrice, closePrice, 14);
-//     let retRSI = talib.RSI(closePrice, 14);
-//     let mfi = retMFI[retMFI.length - 1];
-//     let cci = retCCI[retCCI.length - 1];
-//     let cmo = retCMO[retCMO.length - 1];
-//     let aroonosc = retAROONOSC[retAROONOSC.length - 1];
-//     let adx = retADX[retADX.length - 1];
-//     let rsi = retRSI[retRSI.length - 1];
-//     return _get_signal(mfi, cci, cmo, aroonosc, adx, rsi);
-// }
-//
 
 /////////////////////// Private Method ///////////////////////////////////////////////
 class InfluxShortIIStrategy extends BaseStrategy {
@@ -215,7 +168,7 @@ class InfluxShortIIStrategy extends BaseStrategy {
         let mailOptions = {
             from: process.env.SEND_FROM, // 发件人
             to: process.env.SEND_TO, // 收件人
-            subject: "Action "+ this.name + " signal: " + this.signal, // 主题
+            subject: "Action " + this.name + " signal: " + this.signal, // 主题
             text: message, // plain text body
             html: `<b>${message}</b>`, // html body
         };
